49f0d0a44019efc85cc998bfad98ef39ebc6ff63
Marco Sandri <sandrimarco98@gmail.com>
2022-02-11 16:08:14 +0100

    fine interfaccia

--- main.js
+++ main.js
@@ -7,27 +7,34 @@ const vue = new Vue({
         line : false
         },
         methods: {
+            //Aggiunge elemento alla lista
             addTodo() {
-                if(this.newtodo != "") {
+                //Controlla che la stringa non sia vuota e che non sia composta solamente da spazi
+                //.trim() toglie spazi dall'inizio e fine della stringa
+                if(this.newtodo.trim() != "") {
                     this.todolist.push(this.newtodo);
                     this.newtodo = "";
                 } else {
+                    this.newtodo = "";
                     document.getElementById('app').style.animation = "error 500ms linear 1";
                     setTimeout(this.removeAnimation, 500)
                 }
             },
+            //Elimina elemento dalla lista
             deleteTodo(index) {
                 this.todolist.splice(index, 1)
             },
+            //Sbarra elementi
             done(event) {
                 if(!this.line) {
                     this.line = true;
-                    event.currentTarget.style.textDecoration = "line-through";
+                    event.currentTarget.classList.add("done")
                 } else if(this.line) {
                     this.line = false;
-                    event.currentTarget.style.textDecoration = "none";
+                    event.currentTarget.classList.remove("done")
                 }
             },
+            //Rimuove animazione di errore
             removeAnimation(){
                 document.getElementById('app').style.animation = "";
             }
